1c3b9a6d9834222370fd359e70621358351da1bb
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-03-07 19:28:24 -0300

    feat: added routes and created pages
    Co-authored-by: w2d-ar <contac.w2d@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn'; 
+import SignUp from './pages/SignUp'; 
+import Profile from './pages/Profile';  
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
